3ebd6ee8cbb0ea77885dd7c4f3a9b75494545340
asmaa abdelkhalek <asmaa0abdelkhalek@gmail.com>
2026-07-16 03:13:10 +0300

    resolve error (window is not defined)

--- features/map/components/Map.tsx
+++ features/map/components/Map.tsx
@@ -1,29 +1,11 @@
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import dynamic from "next/dynamic";
 
-const Map = () => {
-  return (
-    <MapContainer
-      center={[30.0444, 31.2357]} //ده الـ Latitude والـ Longitude ارقام ثابته للوكيشن بعد كدا هجبها من api
-      zoom={13}
-      style={{ height: "400px", width: "100%" }}
-    >
-      <TileLayer
-        attribution="&copy; OpenStreetMap contributors"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" //دي الـ URL اللي Leaflet بيجيب منه صور الخريطة
-      />
-    </MapContainer>
-  );
-};
+const MapView = dynamic(() => import("./MapView"), {
+  ssr: false,
+});
 
-export default Map;
-
-// TileLayer;
-
-// الخريطة نفسها عبارة عن صور صغيرة جدًا اسمها Tiles.
-
-// لما تعملي Zoom، المكتبة بتحميل Tiles جديدة.
-
-// فـ TileLayer هو اللي بيجيب الصور دي من OpenStreetMap.
+export default function Map() {
+  return <MapView />;
+}
