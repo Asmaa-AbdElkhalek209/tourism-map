59bf890dc43ef6d9237ffa84483348c60f992e80
asmaa abdelkhalek <asmaa0abdelkhalek@gmail.com>
2026-07-16 03:35:34 +0300

    Display company headquarters(misrtrave) with a marker.

--- features/map/components/MapView.tsx
+++ features/map/components/MapView.tsx
@@ -1,25 +1,36 @@
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
+import "../lib/fixLeafletIcon";
 const MapView = () => {
+  const companyLocation: [number, number] = [30.0444, 31.2357];
   return (
     <MapContainer
-      center={[30.0444, 31.2357]} //ده الـ Latitude والـ Longitude ارقام ثابته للوكيشن بعد كدا هجبها من api
+      center={companyLocation}
       zoom={13}
       style={{ height: "400px", width: "100%" }}
     >
       <TileLayer
         attribution="&copy; OpenStreetMap contributors"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" //دي الـ URL اللي Leaflet بيجيب منه صور الخريطة
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={companyLocation}>
+        <Popup>
+          <div>
+            <h3>برج مصر لسياحة</h3>
+            <p>Cairo, Egypt</p>
+          </div>
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 };
 
 export default MapView;
 
+//  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" //دي الـ URL اللي Leaflet بيجيب منه صور الخريطة
+//center={[30.0444, 31.2357]} //ده الـ Latitude والـ Longitude ارقام ثابته للوكيشن بعد كدا هجبها من api
 // TileLayer;
 
 // الخريطة نفسها عبارة عن صور صغيرة جدًا اسمها Tiles.
